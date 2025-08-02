c6d469b2b04b47bee0ceabdf024d10d06da9ba78
Peter Wangimwa <peterwangimwa@gmail.com>
2025-08-02 17:25:43 +0300

    Fix method names to match expected test cases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,9 +14,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 7), -7)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 5), -10)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
         self.assertIsNone(self.calc.divide(10, 0))
